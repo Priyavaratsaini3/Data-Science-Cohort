ef4ad611431b1fc3b04ad59201a994aedb0a9642
priyavaratsaini3 <priyavartsaini3@gmail.com>
2025-06-04 20:18:30 +0530

    import the modules and reuse the models

---
+++ Python/string_utils.py
@@ -0,0 +1,15 @@
+## reversal of a string 
+def reverse_string(s):
+    return s[::-1]
+
+## check if a string is a palindrome
+def palindrome(s):
+    return s == s[::-1]
+
+## concatenation of two strings
+def concatenation_string(s1,s2):
+    return s1 + s2
+
+## string length 
+def string_length(s):
+    return len(s)
