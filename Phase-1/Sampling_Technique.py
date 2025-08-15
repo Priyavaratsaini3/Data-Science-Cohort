624870a99bf2f90b3899b739aae0b264a5a59506
priyavaratsaini3 <priyavartsaini3@gmail.com>
2025-08-15 16:12:13 +0530

    update

--- Phase-1/Sampling_Technique.py
+++ Phase-1/Sampling_Technique.py
@@ -6,29 +6,29 @@ import seaborn as sns
 
 # read the data in csv file 
 data = pd.read_csv(r"C:\Desktop\Data-Science-Cohort\Phase-1\Population_Survey_Data_lyst1750274390196.csv")
-# # print(data.head())
-# # print(data.shape)
+print(data.head())
+print(data.shape)
 
-# # Simple Random Sampling : 100 Records Randomly 
+# Simple Random Sampling : 100 Records Randomly 
 
-# sample_data = data.sample(n=100, random_state=44)
-# print(sample_data)
+sample_data = data.sample(n=100, random_state=44)
+print(sample_data)
 
-# # Stratified Random Sampling
+# Stratified Random Sampling
 
-# Stratified_Random_data = data.groupby('Region').apply(lambda x : x.sample(min(len(x), 25), random_state=44))
-# print(Stratified_Random_data)
+Stratified_Random_data = data.groupby('Region').apply(lambda x : x.sample(min(len(x), 25), random_state=44))
+print(Stratified_Random_data)
 
 #  Cluster Sampling : Manufacturing Data by Batch Number
-# data = pd.read_csv(r"Phase-1/Manufacturing_Data_lyst1750274395200.csv")
-# cluster = data['BatchNumber'].unique()
-# # print(cluster)
+data = pd.read_csv(r"Phase-1/Manufacturing_Data_lyst1750274395200.csv")
+cluster = data['BatchNumber'].unique()
+print(cluster)
 
-# Selected_cluster = np.random.choice(cluster, size=5, replace=False)
-# print(Selected_cluster)
+Selected_cluster = np.random.choice(cluster, size=5, replace=False)
+print(Selected_cluster)
 
-# clustered_sample = data[data['BatchNumber'].isin(Selected_cluster)]
-# print(clustered_sample)
+clustered_sample = data[data['BatchNumber'].isin(Selected_cluster)]
+print(clustered_sample)
 
 # Systemtic sampling
 systemtic_sample  = data.iloc[::10, :]
